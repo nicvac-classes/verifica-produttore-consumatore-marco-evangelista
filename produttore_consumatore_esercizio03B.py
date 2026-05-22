1835e11f9bceb08568a4aaaa3aafae320c0d1aa0
Marco1908 <evangelistagiovanni814@gmail.com>
2026-05-22 08:23:59 +0000

    verifica

--- produttore_consumatore_esercizio03B.py
+++ produttore_consumatore_esercizio03B.py
@@ -26,7 +26,18 @@ class ProduttoreThread(threading.Thread):
         self.idx = idx
 
     # DA IMPLEMENTARE (run)
-
+    def run(self)
+        global metti
+        while <= N_RICHIESTE:
+            self.vuoto.acquire()
+            self.mutexP.acquire()
+            i_metti=metti
+            self.dato=genera_drone()
+            metti=(metti+1)%DIM_BUFFER
+            self.mutexP.realise()
+            self.buffer[drone]=metti
+            print(f"[SENSOR-N] segnala {metti}")
+            self.pieno.realise()
 
 class ConsumatoreThread(threading.Thread):
     def __init__(self, idx):
@@ -34,7 +45,24 @@ class ConsumatoreThread(threading.Thread):
         self.idx = idx
 
     # DA IMPLEMENTARE (run)
+    def run(self)
+        global togli
+        termina=false
+        while termina(false):
+            self.pieno.acquire()
+            self.mutexC.acquire()
+            i_togli=togli
+            togli=(togli+1)%DIM_BUFFER
+            self.mutexC.realise()
+            togli=self.buffer[i_togli]
+            if togli==None:
+                attiva= false
+            else:
+                print(f"[RUNWAY-N] autorizza atterraggio{togli}")
+            
+            self.vuoto.realise()
 
+                
 
 def main():
     global metti
@@ -43,17 +71,26 @@ def main():
     consumatori = [ConsumatoreThread(i + 1) for i in range(N_CONSUMATORI)]
 
     # DA IMPLEMENTARE: start dei thread produttori e consumatori
+    for start in ProduttoreThread():
+        ProduttoreThread.start()
+    for start in ConsumatoreThread():
+        ConsumatoreThread.start()
 
     # DA IMPLEMENTARE: join di tutti i produttori
+    for join in ProduttoreThread():
+        ProduttoreThread.join()
 
     print("Tutti i sensori hanno terminato. Chiusura piste...")
 
     # Invia una sentinella None per ogni pista attiva.
     for _ in range(N_CONSUMATORI):
         # DA IMPLEMENTARE: inserire None nel buffer
+        buffer=None
         pass
 
     # DA IMPLEMENTARE: join di tutti i consumatori
+    for join in ConsumatoreThread:
+        ConsumatoreThread.join()
 
     print("Torre operativa chiusa.")
 
